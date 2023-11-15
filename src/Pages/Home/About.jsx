05917bcaaa1cb80b54699bb16e04ca451f208e55
shamima-akter <shamimamirpur2@gmail.com>
2023-11-15 22:27:46 +0600

    about section update

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -1,33 +1,59 @@
 const About = () => {
   return (
-    <div className="mb-10 bg-slate-200">
+    <div className="my-10 pb-10 bg-slate-200">
       <div className="hero h-auto lg:h-[30vh] overflow-hidden
                bg-fixed bg-cover bg-center relative"style={{
                backgroundImage: "url(  https://i.ibb.co/80mymPH/office.jpg )",}} >
            <div className="hero-overlay bg-opacity-70">
-               <h1 className="text-3xl md:text-4xl py-2 text-center text-white my-10 font-bold">
+               <h1 className="text-3xl md:text-4xl  text-center text-white my-10 font-bold">
                    About<span className="text-cyan-500"> Us</span>
                </h1>
-               <p className="text-white text-center">Welcome to <span className="font-bold">Job<span className="text-cyan-500" >IN</span></span> where we connect talent with opportunity in the dynamic fields of web development, graphic design, and digital marketing.</p>
+               <p className="text-white text-center mb-3">Welcome to <span className="font-bold">Job<span className="text-cyan-500" >IN</span></span> where we connect talent with opportunity in the dynamic fields of web development, graphic design, and digital marketing.</p>
            </div>
        </div>
  
-       <div className="flex">
-         {/* ---- 1 -------- */}
-          <div className="flex-1">
-                  <div>
-                     <h1>Mission Statement:</h1>
+       <div className="my-6 mx-10">
+            <div className="py-4" >
+                <h1 className="text-2xl font-bold" >Our Mission</h1>
+                <p>At  <span className="font-bold">Job<span className="text-cyan-500" >IN</span></span>, our mission is to empower individuals to build fulfilling careers and assist businesses in finding the right talent to thrive in the digital age. We believe in creating a platform that not only simplifies the job search process but also fosters a community dedicated to continuous learning and professional growth.</p>
+            </div>
 
-                  </div>      
-          </div>
+            <div className="py-4" >
+                <h1 className="text-2xl font-bold" >Who We Are</h1>
+                <p>Founded in 1992,  <span className="font-bold">Job<span className="text-cyan-500" >IN</span></span>  is the brainchild of a passionate team of professionals with a shared vision of bridging the gap between talent and the ever-evolving job market. Our journey began with a commitment to make the job search experience seamless, transparent, and rewarding for both job seekers and employers.</p>
+            </div>
 
-          <div className="flex-1" >
-               <h1>blog</h1>
-          </div>
+            <div className="py-4" >
+                <h1 className="text-2xl font-bold" >What Sets Us Apart</h1>
+                <p>At  <span className="font-bold">Job<span className="text-cyan-500" >IN</span></span> , we stand out through our commitment to user-centric design, innovative features, and a genuine passion for the industries we serve. Our platform goes beyond traditional job listings; it's a dynamic hub that facilitates connections, fosters skill development, and celebrates the successes of our vibrant community.</p>
+            </div>
 
-          <div className="flex-1">
-               <h1>blog</h1>
-          </div>
+            <div className="py-4" >
+                <h1 className="text-2xl font-bold" >Meet the Team</h1>
+                <p>Behind  <span className="font-bold">Job<span className="text-cyan-500" >IN</span></span>  is a diverse team of experts who bring a wealth of experience in web development, graphic design, digital marketing, and technology. We are driven by a collective desire to make a positive impact on the professional journeys of our users.</p>
+            </div>
+
+            <div className="py-4" >
+                <h1 className="text-2xl font-bold" >Transparency and Trust</h1>
+                <p>We prioritize transparency in all our interactions. Your trust is paramount to us, and we take great care in handling your data with the utmost respect for privacy and security.</p>
+            </div>
+
+            <div className="py-4" >
+                <h1 className="text-2xl font-bold" >Our Community</h1>
+                <p><span className="font-bold">Job<span className="text-cyan-500" >IN</span></span> is not just a platform; it is a community of like-minded individuals passionate about shaping the future of the digital landscape. From beginners to seasoned professionals, everyone is welcome to join our thriving community, share insights, and embark on a journey of continuous learning.</p>
+            </div>
+
+            <div className="py-4" >
+                <h1 className="text-2xl font-bold" >Get in Touch</h1>
+                <p>We are here to support you every step of the way. Whether you are a job seeker looking for your next opportunity or an employer seeking top talent, <span className="font-bold">Job<span className="text-cyan-500" >IN</span></span> is your partner in success. Feel free to reach out to us at www.jobin@gmail.com for any inquiries, feedback, or collaboration opportunities.</p>
+            </div>
+
+            <div className="py-4" >
+                <h1 className="" >
+                Thank you for being a part of the <span className="font-bold">Job<span className="text-cyan-500" >IN</span></span> community. Let us build a future full of exciting opportunities together!
+                </h1>
+            </div>
+            
        </div>
 
         
